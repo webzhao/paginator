1be9dd8b41fcb8b5b32f98d5be2a5311d0c46c9c
webzhao <webzhao@gmail.com>
2013-12-31 14:44:41 +0800

    change build dir

--- build/qiwoo/module/Paginator/1.0.0/sidebar.js
+++
@@ -1,9 +0,0 @@
-(function(define, global) { 
-define(['module/module/widget/1.0.0/widget'], function(Widget) {
-Widget = Widget || this.Widget;
-
-
-    function Paginator() {};
-    return Paginator;
-}); 
-}) ( typeof define === 'function' && define.amd ? define : function (name, requires, factory) { if(typeof name === 'function') { factory = name; } else if(typeof requires === 'function') { factory = requires; } if(typeof module != 'undefined'){ module.exports = factory(require); }else if(typeof window != 'undefined'){ window.Sidebar= factory(); } }, this);
